fb2b281d21dc2dc8676f2d772e514e3ddb816d79
Alexey-t <support@uvviewsoft.com>
2019-02-09 21:04:22 +0300

    init

--- __init__.py
+++ __init__.py
@@ -15,12 +15,12 @@ class Command:
     
     def __init__(self):
 
-        self.h_dlg = self.init_dlg()
+        self.init_dlg()
         
     def open_dlg(self):
 
         self.update_combo()
-        self.update_list()
+        self.callback_btn_change(0, 0)
         
         title = 'Snippet Panel'
         app_proc(PROC_SIDEPANEL_ADD_DIALOG, (title, self.h_dlg, fn_icon) )
@@ -32,11 +32,14 @@ class Command:
         dlg_proc(h, DLG_PROP_SET, prop={
             #'on_key_down': 'cuda_testing_dlg_proc.callback_tempdlg_on_key_down',
             })
+        self.h_dlg = h
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button_ex')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={
             'name': 'btn',
             'align': ALIGN_TOP,
+            'act': True,
+            'on_change': self.callback_btn_change,
             })
         self.h_btn = dlg_proc(h, DLG_CTL_HANDLE, index=n)
         
@@ -48,15 +51,21 @@ class Command:
         self.h_list = dlg_proc(h, DLG_CTL_HANDLE, index=n)
         
         button_proc(self.h_btn, BTN_SET_KIND, BTNKIND_TEXT_CHOICE)
+        
         listbox_proc(self.h_list, LISTBOX_THEME)
             
-        return h
 
     def update_combo(self):
     
-        pass
+        self.clips = sorted(os.listdir(dir_clips))
+        button_proc(self.h_btn, BTN_SET_ITEMS, '\n'.join(self.clips))
+        button_proc(self.h_btn, BTN_SET_ITEMINDEX, 0)
     
     def update_list(self):
     
         pass
         
+    def callback_btn_change(self, id_dlg, id_ctl, data='', info=''):
+
+        id = button_proc(self.h_btn, BTN_GET_ITEMINDEX)
+        print(self.clips[id])
